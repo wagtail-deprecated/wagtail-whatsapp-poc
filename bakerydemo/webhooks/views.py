70700deedf3b1ac1349413fb589210f2ebeb96a9
Codie Roelf <codiebeulaine@gmail.com>
2019-03-13 14:56:05 +0100

    endpoint for demo

--- bakerydemo/webhooks/views.py
+++ bakerydemo/webhooks/views.py
@@ -1,4 +1,48 @@
+import requests
+import json
 from django.http import HttpResponse
+from wagtail.core.models import Page
+
 
 def whatsapp(request):
+    token = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJFbmdhZ2VkIiwiZXhwIjoxNTU0MDc2Nzk5LCJpYXQiOjE1NTI0ODA5OTEsImlzcyI6IkVuZ2FnZWQiLCJqdGkiOiIxMjQyNjk4ZC0yMmM1LTQ4ZGYtYmJiYi1lMTE3NmFhNjVhNDYiLCJuYmYiOjE1NTI0ODA5OTAsInN1YiI6Im51bWJlcjoxNDMiLCJ0eXAiOiJhY2Nlc3MifQ.Jaj1zkgpjJQEQlgZxu5WV16IbXYu7Sjw-sskOnO7q7DMkZiYc79tBXlMK3yZ_453vz4UA4nOsPWjxWkxGhLtFQ'
+    url = 'https://whatsapp.praekelt.org/v1/messages'
+    if request.GET.get('text', None) and request['text'] == 'search':
+        # blah blah
+        pass
+    else:
+        if request.GET.get('text', None):
+            data = {
+                "preview_url": False,
+                "recipient_type": "individual",
+                "to": "xxx",
+                "type": "text",
+                "text": {
+                    "body": Page.objects.get(live=True, title=request['text']).title
+                }
+            }
+            headers={
+                'Authorization': 'Bearer %s' % token,
+                'Content-Type': 'application/json'
+            }
+            response = requests.post(
+                url, data=data, headers=headers)
+        else:
+            data = {
+                "preview_url": False,
+                "recipient_type": "individual",
+                "to": "xxx",
+                "type": "text",
+                "text": {
+                    "body": "Please send the word search to begin."
+                }
+            }
+            headers={
+                'Authorization': 'Bearer %s' % token,
+                'Content-Type': 'application/json'
+            }
+            response = requests.post(
+                url=url, data=json.dumps(data), headers=headers)
+            return HttpResponse(response)
+
     return HttpResponse('ok!')
