ac1458e9d2c43d613d10e522a7ef720f11a206a2
Codie Roelf <codiebeulaine@gmail.com>
2019-03-13 16:25:57 +0100

    take away look for search keyword

--- bakerydemo/webhooks/views.py
+++ bakerydemo/webhooks/views.py
@@ -11,44 +11,50 @@ from bakerydemo.base.models import StandardPage
 def whatsapp(request):
     token = settings.WEBHOOKS_WHATSAPP_TOKEN
     url = 'https://whatsapp.praekelt.org/v1/messages'
-    message = request.body['messages']['body']
-    contact = request.body["contacts"][0]["wa_id"]
-    if message and message == 'search':
-        # blah blah
-        pass
+    try:
+        body = json.loads(request.body.decode('ASCII'))
+        message = body["messages"][0]["text"]["body"]
+        contact = body["contacts"][0]["wa_id"]
+    except:
+        return HttpResponse('No body in request')
+    if message:
+        try:
+            # TODO: use actual search API here for more relevant results
+            body = StandardPage.objects.get(title__icontains==request['text']).introduction
+        except:
+            body = 'We could not find an Article matching that keyword. Please type in a different keyword...'
+        data = {
+            "preview_url": False,
+            "recipient_type": "individual",
+            "to": contact,
+            "type": "text",
+            "text": {
+                "body": body
+            }
+        }
+        headers={
+            'Authorization': 'Bearer %s' % token,
+            'Content-Type': 'application/json'
+        }
+        response = requests.post(
+            url, data=json.dumps(data), headers=headers)
+        return HttpResponse(response)
     else:
-        if message:
-            data = {
-                "preview_url": False,
-                "recipient_type": "individual",
-                "to": contact,
-                "type": "text",
-                "text": {
-                    "body": StandardPage.objects.get(live=True, title=request['text']).introduction
-                }
+        data = {
+            "preview_url": False,
+            "recipient_type": "individual",
+            "to": contact,
+            "type": "text",
+            "text": {
+                "body": "Please type in keyword to search..."
             }
-            headers={
-                'Authorization': 'Bearer %s' % token,
-                'Content-Type': 'application/json'
-            }
-            response = requests.post(
-                url, data=data, headers=headers)
-        else:
-            data = {
-                "preview_url": False,
-                "recipient_type": "individual",
-                "to": contact,
-                "type": "text",
-                "text": {
-                    "body": "Please send the word search to begin."
-                }
-            }
-            headers={
-                'Authorization': 'Bearer %s' % token,
-                'Content-Type': 'application/json'
-            }
-            response = requests.post(
-                url=url, data=json.dumps(data), headers=headers)
-            return HttpResponse(response)
+        }
+        headers={
+            'Authorization': 'Bearer %s' % token,
+            'Content-Type': 'application/json'
+        }
+        response = requests.post(
+            url=url, data=json.dumps(data), headers=headers)
+        return HttpResponse(response)
 
-    return HttpResponse('ok!')
+    return HttpResponse('I did nothing :/')
